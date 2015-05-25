4a0b56bf44868387ce06fa3324e62a57b0a56e8e
Randy Fischer <fischer@sacred.net>
2015-05-24 20:56:45 -0400

    starting to follow rubocop's advice, sigh

--- repl.rb
+++ repl.rb
@@ -4,49 +4,50 @@ require 'command-executor'
 
 class ReplError < StandardError; end
 
-$stdout.sync = true                    # REPL needs immediate flush.   stdout?
-@@DEBUG      = false
-
 class Repl
+  @@debug      = false
+  $stdout.sync = true                    # REPL needs immediate flush.   stdout?
 
-  def initialize  command_exectutor, prompt = "repl> ", goodbye_message = "Thanks for all the fish!"
+  def initialize(command_exectutor, prompt = 'repl> ', goodbye_message = 'Thanks for all the fish!')
     @commands = command_exectutor
     @prompt   = prompt
     @goodbye  = goodbye_message
-    raise ReplError, "The first argument must be a CommandExecutor object" unless @commands.is_a? CommandExecutor
+    fail ReplError, 'The first argument must be a CommandExecutor object' unless @commands.is_a? CommandExecutor
   end
 
   # Top level REPL (Read-Eval-Print Loop).
 
-  def loop
-    while true
+  def repl
+    loop do
       write(@prompt)
-      command = get()
+      command = get
       goodbye if command.nil?
       response = execute(command)
-      writeln(response.chomp) unless response.nil? or response.empty?
+      writeln(response.chomp) unless response.nil? || response.empty?
     end
   rescue => e
     exception_message(e)
     retry
   end
 
-  def debug= val
-    @@DEBUG = val
+  def debug=(val)
+    @@debug = val
   end
+
+  # write, writeln, get, execute are provided for subclassing
 
   # The 'P' in the REPL: write(text, text...) ship out supplied text
   # strings.
 
-  def write *text
-    $stdout.write text.flatten.join() unless text.empty?
+  def write(*text)
+    $stdout.write text.flatten.join unless text.empty?
   end
 
-  def writeln *text
+  def writeln(*text)
     write text, "\n"  unless text.empty?
   end
 
-  # The 'R' in the REPL: get() - reads and cleans up input, exiting if
+  # The 'R' in the REPL: get - reads and cleans up input, exiting if
   # EOF has been reached (^D is entered, normally).  Input is returned
   # otherwise.
 
@@ -59,14 +60,14 @@ class Repl
   # The 'E' in the REPL is mostly done by the CommandExecutor object
   # method CommandExecutor#execute(String).
 
-  def execute command
+  def execute(command)
     return @commands.execute(command)
   end
 
   private
 
   def debug
-    @@DEBUG
+    @@debug
   end
 
   def goodbye
@@ -74,9 +75,9 @@ class Repl
     exit
   end
 
-  def exception_message exception
+  def exception_message(exception)
     writeln @commands.usage, "\n", exception.message
     writeln exception.class, "\n", exception.backtrace.join("\n") if debug
   end
 
-end
+end # of class Repl
